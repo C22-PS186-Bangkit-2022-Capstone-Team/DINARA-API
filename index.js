44770dc3b8435f81c80d1f4a1f76f966de4e76d2
starifphlwrelf <starifphlwrelf@gmail.com>
2022-06-12 16:22:18 +0700

    ci/cd

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
+require('dotenv').config()
+
 const express = require("express");
 const mysql = require("mysql");
-const Pool = require("mysql/lib/Pool");
-const app = express();
 
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database')
 
 app.use(express.json());
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`DINARA REST API listening on port ${port}`);
+    console.log(`DINARA REST API is running at:http://localhost:${port}`);
 });
 
 app.get("/", async (req, res) => {
     res.json({ status: "working properly!" });
 });
 
-app.get("/:nama", async (req, res) => {
-    const query = "SELECT * FROM monument WHERE nama =?";
-    pool.query(query, [req.params.nama], (error, results) => {
-        if (!results[0]) {
-            res.json({ status: " Data Not Found!" });
-        } else {
-            res.json(results[0]);
+app.get('/monument', (req, res) => {
+    connection.query(
+        "SELECT * FROM `dinaradb`.`monument`",
+        (error, results, fields) => {
+            if (error) throw error;
+            res.json(results);
         }
+    );
+});
+
+app.route("/monument/:id")
+    .get((req, res, next) => {
+        connection.query(
+            "SELECT * FROM `dinaradb`.`monument` WHERE id = ?", req.params.id,
+            (error, results, fields) => {
+                if (error) throw error;
+                res.json(results);
+            }
+        );
     });
-});
 
 
-const pool = mysql.createPool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    path: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
-});
+
